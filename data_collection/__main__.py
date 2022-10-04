961a83854aac053a5c38eb3156794334f129be58
trevor_pearce <trevor.r.pearce@gmail.com>
2022-10-03 23:18:14 -0400

    test push

--- data_collection/__main__.py
+++ data_collection/__main__.py
@@ -1,18 +1,25 @@
 import logging
-from statistics import quantiles
 from time import sleep
 #from scraping.scraper_app import ScraperApp
 from scraping.scrapers.cnn_scraper import CnnScraper
+from settings import AppConfig
 
-logging.basicConfig(level=logging.INFO)
+import os 
+from utils import parse_cmd_line_args_to_env_vars, set_env_var_defaults
 
+logging.basicConfig(level=AppConfig.LOGGING_LEVEL)
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    sleep(10)
-    scraper = CnnScraper()
-    rendered_html = scraper.run()
-    logger.info(rendered_html)
-    sleep(600) 
+    set_env_var_defaults()
+    parse_cmd_line_args_to_env_vars()
+    
+    sleep(5)
+    try:
+        scraper = CnnScraper(selenium_endpoint=os.environ['SELENIUM_ENDPOINT'])
+    except KeyError: 
+        logging.error('You probably forgot to set env var "SELENIUM_ENDPOINT"')
+    scraper.run()
+    sleep(3) 
     #app = ScraperApp()
     #app.run()
